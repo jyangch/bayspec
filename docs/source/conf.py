dcb67a26cdfe4747b196ac65d9fc52086e45d8e9
jyangch <jyang.0086@gmail.com>
2024-10-24 18:29:50 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,5 +27,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
