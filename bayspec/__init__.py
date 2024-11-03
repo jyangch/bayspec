46a858ace07c4f08673b180b7f0364e23ca1ab02
jyangch <jyang.0086@gmail.com>
2024-11-03 17:57:28 +0800

    update

--- bayspec/__init__.py
+++ bayspec/__init__.py
@@ -7,4 +7,3 @@ from .__info__ import __version__
 
 from os.path import dirname, abspath
 __app__ = dirname(abspath(__file__)) + '/app/app.py'
-__readme__ = dirname(dirname(abspath(__file__))) + '/README.md'
