23c25f4e9aa394436fcadc1e4f191fe63fa70e3a
Jun Yang <jyang.astro@gmail.com>
2026-04-20 20:24:29 +0800

    refactor: improve docstrings and comments for clarity in user model

--- bayspec/model/user/user.py
+++ bayspec/model/user/user.py
@@ -22,65 +22,49 @@ class user(Model):
     def __init__(self):
         """Initialise the template with a single uniform parameter ``p1``."""
         super().__init__()
-        
+
         #+++++++editable area+++++++
-        # name of your model
         self.expr = 'model'
-        # type of your model, optional
-        # ('add', 'mul', 'tinv', 'math')
-        self.type = 'add'
-        # comment of your model
+        self.type = 'add'  # 'add' | 'mul' | 'tinv' | 'math'
         self.comment = 'user-defined model'
         #+++++++editable area+++++++
-        
+
         self.config = OrderedDict()
         #+++++++editable area+++++++
-        # set your model configuration
         self.config['redshift'] = Cfg(0)
-        # above sentence define redshift 
-        # with value of 0
         #+++++++editable area+++++++
-        
+
         self.params = OrderedDict()
         #+++++++editable area+++++++
-        # set your model parameters
         self.params['p1'] = Par(1, unif(0, 2))
-        # above sentence define a parameter p1 
-        # with value of 1 
-        # with prior of uniform within [0, 2]
         #+++++++editable area+++++++
 
 
     def func(self, E, T=None, O=None):
+        """Evaluate the model spectrum at energies ``E``.
+
+        Args:
+            E: Energy grid in keV; redshift correction is applied internally.
+            T: Time array in seconds; required for ``tinv``-type models.
+            O: Nested model passed by a convolution operator; unused here.
+
+        Returns:
+            Photon spectrum N(E, T) in photons/cm²/s/keV for ``add``/``tinv``
+            types, or dimensionless F(E, T) for ``mul``/``math`` types.
         """
-        Parameters
-        ----------
-        E: energy array in keV
-        T: time array in second
-        O: useless now
-        Returns
-        -------
-        if type is add or tinv:
-        photon spectrum N(E, T) in photons/cm2/s/keV
-        if type is mul or math:
-        dimensionless F(E, T)
-        """
-        #+++++++editable area+++++++
-        # get the value of model configuration
-        redshift = self.config['redshift'].value
-        #+++++++editable area+++++++
         
         #+++++++editable area+++++++
-        # get the value of model parameter
+        redshift = self.config['redshift'].value
+        #+++++++editable area+++++++
+
+        #+++++++editable area+++++++
         p1 = self.params['p1'].value
         #+++++++editable area+++++++
 
-        # apply redshift correction
         zi = 1 + redshift
         E = E * zi
 
         #+++++++editable area+++++++
-        # code your model
         phtspec = E ** p1 + T
         #+++++++editable area+++++++
 
